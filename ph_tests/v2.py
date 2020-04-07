b2fe68739076988e0c97beb7171b967266c19ab3
Paulo Henrique <paulohfcmendes@gmail.com>
2020-04-07 11:12:37 -0300

    feat/ author-ID table

--- ph_tests/v2.py
+++ ph_tests/v2.py
@@ -3,6 +3,7 @@ import csv
 
 authorsVector = list()
 authorsList = list()
+authorsListId = list()
 relationVector = []
 teste = ["A", "B", "C"]
 teste2 = ["Paulo", "Aurelio", "Dalton", "André", "Ayrton", "Julio", "Aline"]
@@ -20,11 +21,16 @@ end = len(authorsVector)
 
 # Cria o relacionamento de todos com todos para a produção
 for j in range(end):
-    print(j)
     for item in authorsVector:
         if(authorsVector[j] != item and item != ''):
             relationVector.append([authorsVector[j], item])
     authorsVector[j] = ''
 
+# Atribui um id para cada autor
+i = 0
+for item in teste:
+    authorsListId.append([teste[i], i+1])
+    i += 1
 
-print(relationVector)
+
+print(authorsListId)
