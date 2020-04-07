87edfb0d2049f6a303ce5b5c12f4aba4fe25d824
Paulo Henrique <paulohfcmendes@gmail.com>
2020-04-07 15:16:35 -0300

    feat/ networkx

--- ph_tests/v2.py
+++ ph_tests/v2.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import csv
+import networkx as nx
+import matplotlib.pyplot as plt
+
 
 authorsVector = list()
-authorsList = list()
+authorsVectorBackup = []
 authorsListId = list()
 relationVector = []
 teste = ["A", "B", "C"]
@@ -11,11 +14,19 @@ teste2 = ["Paulo", "Aurelio", "Dalton", "André", "Ayrton", "Julio", "Aline"]
 # Importa arquivo .csv
 scopus_csv = pd.read_csv('teste.csv')
 
+
 # Desmebra a coluna de "Authors" e cria um vetor com os autores
 for line in scopus_csv["Authors"]:
     authors = line.split(',')
     for author in authors:
         authorsVector.append(author)
+
+
+# Atribui um id e afiliação para cada autor || nome_autor | id_autor | affil_autor||
+i = 0
+for item in authorsVector:
+    authorsListId.append([authorsVector[i], i+1])
+    i += 1
 
 end = len(authorsVector)
 
@@ -26,11 +37,19 @@ for j in range(end):
             relationVector.append([authorsVector[j], item])
     authorsVector[j] = ''
 
-# Atribui um id para cada autor
-i = 0
-for item in teste:
-    authorsListId.append([teste[i], i+1])
-    i += 1
 
+f = open('gephi.csv', 'w')
+with f:
+    writer = csv.writer(f)
+    writer.writerow(['author', 'coauthor'])
+    for row in relationVector:
+        writer.writerow(row)
 
-print(authorsListId)
+df = pd.read_csv("gephi.csv")
+df.head()
+
+graph = nx.from_pandas_edgelist(
+    df, source='author', target='coauthor', edge_attr=None, create_using=None)
+
+nx.draw(graph)
+plt.show()
