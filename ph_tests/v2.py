05605eecabf52e4a9d48070c00234a0872d70d85
Paulo Henrique <paulohfcmendes@gmail.com>
2020-04-07 21:14:35 -0300

    feat/ dinamic node table

--- ph_tests/v2.py
+++ ph_tests/v2.py
@@ -2,54 +2,41 @@ import pandas as pd
 import csv
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
-
-authorsVector = list()
-authorsVectorBackup = []
-authorsListId = list()
+authorsAffiliation = []
 relationVector = []
 teste = ["A", "B", "C"]
 teste2 = ["Paulo", "Aurelio", "Dalton", "André", "Ayrton", "Julio", "Aline"]
-
+author_id = 0
 # Importa arquivo .csv
 scopus_csv = pd.read_csv('teste.csv')
+scopus_df = pd.DataFrame(scopus_csv)
 
+lastRow = len(scopus_csv)
 
-# Desmebra a coluna de "Authors" e cria um vetor com os autores
-for line in scopus_csv["Authors"]:
-    authors = line.split(',')
+for k in range(lastRow):
+    authorsVector = []
+    authorsListId = []
+    authorsAffiliation = []
+
+    # Desmembra a coluna de "Authors" e cria um vetor com os autores
+    authLine = scopus_csv["Authors"][k]
+    authors = authLine.split(',')
     for author in authors:
         authorsVector.append(author)
 
+    # Atribui um id e afiliação para cada autor || nome_autor | id_autor | affil_autor||
+    for l in range(len(authorsVector)):
+        print(l)
+        author_id += 1
+        authorsListId.append([authorsVector[l], author_id])
 
-# Atribui um id e afiliação para cada autor || nome_autor | id_autor | affil_autor||
-i = 0
-for item in authorsVector:
-    authorsListId.append([authorsVector[i], i+1])
-    i += 1
+    print("VALOR", authorsListId)
 
-end = len(authorsVector)
-
-# Cria o relacionamento de todos com todos para a produção
-for j in range(end):
-    for item in authorsVector:
-        if(authorsVector[j] != item and item != ''):
-            relationVector.append([authorsVector[j], item])
-    authorsVector[j] = ''
-
-
-f = open('gephi.csv', 'w')
-with f:
-    writer = csv.writer(f)
-    writer.writerow(['author', 'coauthor'])
-    for row in relationVector:
-        writer.writerow(row)
-
-df = pd.read_csv("gephi.csv")
-df.head()
-
-graph = nx.from_pandas_edgelist(
-    df, source='author', target='coauthor', edge_attr=None, create_using=None)
-
-nx.draw(graph)
-plt.show()
+    # Escreve .cvs de nós
+    f = open('nodes.csv', 'a')
+    with f:
+        writer = csv.writer(f)
+        for row in authorsListId:
+            writer.writerow(row)
